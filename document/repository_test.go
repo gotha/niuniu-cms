fe655657b96ae625fde90d7a755e132fd98406d4
Hristo Georgiev <h.georgiev@hotmail.com>
2021-05-03 00:33:13 +0300

    Add integration tests for document repository GetAllByTag

--- document/repository_test.go
+++ document/repository_test.go
@@ -241,8 +241,18 @@ func TestRepoGetAll(t *testing.T) {
 func TestRepoGetNumDocumentsWithTag(t *testing.T) {
 	conn := getDBConn()
 
-	docUUIDs := []interface{}{uuid.New(), uuid.New(), uuid.New(), uuid.New()}
-	tagUUIDs := []interface{}{uuid.New(), uuid.New(), uuid.New(), uuid.New()}
+	docUUIDs := []interface{}{
+		"11604a76-ed57-4389-a4bf-f0f20e41b73c",
+		"1db47be4-3ab8-446d-a679-e0c6e8873b6c",
+		"c86f2aee-240c-4440-b212-52506717cefe",
+		"6ecbd1c4-64b0-4a33-9f79-6ac50f3c85bc",
+	}
+	tagUUIDs := []interface{}{
+		"1533c1a6-8c9b-430a-9937-de7ac6e9af57",
+		"d2c9e5b4-bbdd-4137-a83c-f487de109207",
+		"5cd3c8e6-6a02-40ad-afb0-634e1f41f0d2",
+		"bdf64f7f-2433-45be-bf9c-d912dd93d99a",
+	}
 
 	queries := []string{
 		fmt.Sprintf(`
@@ -331,6 +341,150 @@ func TestRepoGetNumDocumentsWithTag(t *testing.T) {
 
 			if res != test.expectedNum {
 				t.Fatalf("expected %d results, but got %d", test.expectedNum, res)
+			}
+		})
+	}
+}
+
+func TestRepoGetAllByTag(t *testing.T) {
+	conn := getDBConn()
+
+	docUUIDs := []interface{}{
+		"11604a76-ed57-4389-a4bf-f0f20e41b73c",
+		"1db47be4-3ab8-446d-a679-e0c6e8873b6c",
+		"c86f2aee-240c-4440-b212-52506717cefe",
+		"6ecbd1c4-64b0-4a33-9f79-6ac50f3c85bc",
+	}
+	tagUUIDs := []interface{}{
+		"1533c1a6-8c9b-430a-9937-de7ac6e9af57",
+		"d2c9e5b4-bbdd-4137-a83c-f487de109207",
+		"5cd3c8e6-6a02-40ad-afb0-634e1f41f0d2",
+		"bdf64f7f-2433-45be-bf9c-d912dd93d99a",
+	}
+
+	queries := []string{
+		fmt.Sprintf(`
+		INSERT INTO documents (id, created_at, updated_at, title)
+		VALUES
+			( '%s', '2021-02-01 12:52:00.000 +00:00', '2021-05-05 12:52:00.000 +00:00', 'title4'),
+			( '%s', '2021-03-01 12:52:00.000 +00:00', '2021-05-03 12:52:00.000 +00:00', 'title3'),
+			( '%s', '2021-04-01 12:52:00.000 +00:00', '2021-05-04 12:52:00.000 +00:00', 'title2'),
+			( '%s', '2021-05-01 12:52:00.000 +00:00', '2021-05-01 12:52:00.000 +00:00', 'title1')
+			`, docUUIDs...),
+		fmt.Sprintf(`
+		INSERT INTO tags (id, title)
+		VALUES
+			( '%s', 'tag1'),
+			( '%s', 'tag2'),
+			( '%s', 'tag3'),
+			( '%s', 'tag4')
+		`, tagUUIDs...),
+		fmt.Sprintf(`
+		INSERT INTO document_tags ( tag_id, document_id)
+		VALUES
+			('%s', '%s'),
+			('%s', '%s'),
+			('%s', '%s'),
+			('%s', '%s'),
+			('%s', '%s'),
+			('%s', '%s'),
+			('%s', '%s')`,
+			tagUUIDs[0], docUUIDs[0],
+			tagUUIDs[0], docUUIDs[1],
+			tagUUIDs[0], docUUIDs[2],
+			tagUUIDs[1], docUUIDs[2],
+			tagUUIDs[1], docUUIDs[3],
+			tagUUIDs[2], docUUIDs[0],
+			tagUUIDs[2], docUUIDs[3],
+		),
+	}
+	for _, q := range queries {
+		conn.Exec(q)
+	}
+	defer cleanTables()
+
+	repo := NewRepository(conn)
+
+	tests := []struct {
+		name        string
+		limit       *int
+		offset      *int
+		sortBy      *string
+		sortDesc    *bool
+		tagIDs      []string
+		expectedIDs []string
+	}{
+		{
+			name:        "test that no results are returned when no tags are specified",
+			expectedIDs: []string{},
+		},
+		{
+			name:        "test that no documents are returned when the tag does not exist",
+			tagIDs:      []string{uuid.New().String()},
+			expectedIDs: []string{},
+		},
+		{
+			name:        "test that no documents are returned when tag does not have documents",
+			tagIDs:      []string{fmt.Sprintf("%s", tagUUIDs[3])},
+			expectedIDs: []string{},
+		},
+		{
+			name:   "test that limit is respected when calling tag1 who has 3 documents",
+			tagIDs: []string{fmt.Sprintf("%s", tagUUIDs[0])},
+			limit:  pofi(2),
+			expectedIDs: []string{
+				fmt.Sprintf("%s", docUUIDs[2]),
+				fmt.Sprintf("%s", docUUIDs[1]),
+			},
+		},
+		{
+			name:     "test that sorting works in ASC order",
+			limit:    pofi(5),
+			sortBy:   pofs("title"),
+			sortDesc: pofb(false),
+			tagIDs:   []string{fmt.Sprintf("%s", tagUUIDs[0])},
+			expectedIDs: []string{
+				docUUIDs[2].(string),
+				docUUIDs[1].(string),
+				docUUIDs[0].(string),
+			},
+		},
+		{
+			name:   "test that offset works with default sorting",
+			limit:  pofi(2),
+			offset: pofi(1),
+			tagIDs: []string{fmt.Sprintf("%s", tagUUIDs[0])},
+			expectedIDs: []string{
+				docUUIDs[1].(string),
+				docUUIDs[0].(string),
+			},
+		},
+		{
+			name:     "test that offset works with custom sorting",
+			limit:    pofi(2),
+			sortBy:   pofs("updated_at"),
+			sortDesc: pofb(false),
+			tagIDs:   []string{fmt.Sprintf("%s", tagUUIDs[1])},
+			expectedIDs: []string{
+				docUUIDs[3].(string),
+				docUUIDs[2].(string),
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			res, err := repo.GetAllByTag(test.tagIDs, test.limit, test.offset, test.sortBy, test.sortDesc)
+			if err != nil {
+				t.Fatalf("unexpected error: %s", err)
+			}
+
+			resIDs := []string{}
+			for i := range res {
+				resIDs = append(resIDs, res[i].ID.String())
+			}
+			if !reflect.DeepEqual(resIDs, test.expectedIDs) {
+				t.Fatalf("expected ids: '%v', got '%v'", test.expectedIDs, resIDs)
 			}
 		})
 	}
